d1a4dd0edca6dee4658a7fbd9e0f21dc631cd6d6
MahmoudSalah02 <mahhmouds343@gmail.com>
2022-07-31 12:35:00 +0300

    fixed a bug in buil_database when creating a payment by adding flush

--- seeds/build_database.py
+++ seeds/build_database.py
@@ -4,12 +4,11 @@ from datetime import datetime
 
 items = []
 for item in MENU:
-    print(item)
-    i = Item(name=item.get("name"),
-             price=item.get("price"),
-             ingredients=item.get("ingredients"))
-    items += [i]
-    session.add(i)
+    new_item = Item(name=item.get("name"),
+                    price=item.get("price"),
+                    ingredients=item.get("ingredients"))
+    items += [new_item]
+    session.add(new_item)
 
 # synchronize session data with the database
 session.flush()
@@ -17,32 +16,37 @@ session.flush()
 # iterate over the MENU and CUSTOMERS structure and populate the database
 price = 10
 for customer in CUSTOMERS:
-    c = Customer(id=customer.get("customer_id"),
-                 name=customer.get("name"),
-                 contact_number=customer.get("contact_number"))
-    session.add(c)
+    new_customer = Customer(id=customer.get("customer_id"),
+                            name=customer.get("name"),
+                            contact_number=customer.get("contact_number"))
+    session.add(new_customer)
     for order in customer.get("orders"):
         price += 1.5
         employee_info = order.get("employee")
-        e = Employee(id=employee_info.get("employee_id"),
-                     name=employee_info.get("name"),
-                     contact_number=employee_info.get("contact_number"))
-        o = Order(id=order.get("id"),
-                  customer_id=c.id,
-                  employee_id=e.id,
-                  order_time=datetime.now(),
-                  status=order.get("status"))
-        session.add(e)
-        c.orders_placed.append(o)
+        new_employee = Employee(id=employee_info.get("employee_id"),
+                                name=employee_info.get("name"),
+                                contact_number=employee_info.get("contact_number"),
+                                role="cashier")
+        new_order = Order(id=order.get("id"),
+                          customer_id=new_customer.id,
+                          employee_id=new_employee.id,
+                          order_time=datetime.now(),
+                          status=order.get("status"))
+        session.add(new_employee)
+        new_customer.orders_placed.append(new_order)
         for item in items:
-            orderItem = OrderItem(order_id=o.id,
+            orderItem = OrderItem(order_id=new_order.id,
                                   item_id=item.id,
                                   quantity=1,
                                   customer_notes="no salt")
-            o.items_ordered.append(orderItem)
-        p = Payment(order_id=o.id,
-                    customer_id=c.id,
-                    employee_id=e.id,
-                    price=price)
+            new_order.items_ordered.append(orderItem)
+
+        session.flush()
+
+        new_payment = Payment(order_id=new_order.id,
+                              customer_id=new_customer.id,
+                              employee_id=new_employee.id,
+                              price=price)
+        session.add(new_payment)
 
 session.commit()
